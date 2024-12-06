c606047d356d5e807aaf22c5663cbbf21332d186
JP Ungaretti <jp@jungaretti.com>
2024-12-05 23:36:07 -0800

    Solve day 1 part 1

--- solvers/day01.go
+++ solvers/day01.go
@@ -1,9 +1,48 @@
 package solvers
 
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
 type Day1Solver struct{}
 
 func (d Day1Solver) Day1(input []string) (string, error) {
-	return "5", nil
+	leftValues := []int{}
+	rightValues := []int{}
+
+	for _, line := range input {
+		parts := strings.Fields(line)
+		left, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return "", err
+		}
+		right, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return "", err
+		}
+
+		leftValues = append(leftValues, left)
+		rightValues = append(rightValues, right)
+	}
+
+	sort.Ints(leftValues)
+	sort.Ints(rightValues)
+
+	result := 0
+	for i := 0; i < len(leftValues); i++ {
+		left := leftValues[i]
+		right := rightValues[i]
+
+		if left < right {
+			result += (right - left)
+		} else {
+			result += (left - right)
+		}
+	}
+
+	return strconv.Itoa(result), nil
 }
 
 func (d Day1Solver) Day2(input []string) (string, error) {
